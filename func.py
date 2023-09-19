355ee11247952eb74d8878331d481418297e3d0c
Yura <Orl0ff.1ur@yandex.ru>
2023-09-19 17:55:30 +0300

    create fun

--- func.py
+++ func.py
@@ -17,6 +17,7 @@ def get_hh_data_employers(keyword):
         list_employers.append(item)
     return list_employers
 
+
 def get_employer_id():
     """id работодателей"""
     EMPLOYERS_ID = []
@@ -27,10 +28,12 @@ def get_employer_id():
             EMPLOYERS_ID.append(emloyer["id"])
     return EMPLOYERS_ID
 
+
 def add_json_employers(list_vacancy):
     """Добавление списка работодателей в json"""
     with open('employers.json', 'w', encoding='utf-8') as f:
         json.dump(list_vacancy, f, ensure_ascii=False)
+
 
 def get_hh_data(employer_id):
     """Получает данные с hh.ru по API, принимает номер компании"""
@@ -53,8 +56,7 @@ def add_json_vacancy(list_vacancy):
     метод довавления вакансий в json-файл
     """
     with open('vacancy.json', 'w', encoding='utf-8') as f:
-        json.dump(list_vacancy, f, ensure_ascii=False)
-
+        json.dump(list_vacancy, f)
 
 def delete_json():
     open('vacancy.json', 'w').close()
@@ -62,14 +64,27 @@ def delete_json():
 
 def insert_data_to_tables(cur):
     """функция для наполнения таблиц данными"""
-    with open('employers.json') as json_file:
-        employers = json.load(json_file)
-        for employer in employers:
-            employer_id = employer['id']
-            name = employer['name']
-            vacancies_url = employer['vacancies_url']
-            open_vacancies = employer['open_vacancies']
-        cur.execute("INSERT INTO suppliers VALUES (%s, %s, %s, %s)",
-                    (employer_id, name, vacancies_url, open_vacancies))
+    try:
+        with open('employers.json') as json_file:
+            employers = json.load(json_file)
+            for employer in employers:
+                employer_id = employer['id']
+                company_name = employer['name']
+                vacancies_url = employer['vacancies_url']
+                open_vacancies = employer['open_vacancies']
+            cur.execute("INSERT INTO suppliers VALUES (%s, %s, %s, %s)",
+                        (employer_id, company_name, vacancies_url, open_vacancies))
 
-
+        with open('vacancy.json.json') as json_file:
+            vacancies = json.load(json_file)
+            for vacancy in vacancies:
+                vacancy_id = vacancy['id']
+                vacancy_name = vacancy['name']
+                salary_from = vacancy['salary_from']
+                salary_to = vacancy['salary_to']
+                employer_id = vacancy['employer']['id']
+                url = vacancy['alternate_url']
+            cur.execute("INSERT INTO suppliers VALUES (%s, %s, %s, %s, %s, %s)",
+                        (vacancy_id, employer_id, vacancy_name, salary_from, salary_to, url))
+    except:
+        ""
